4a75ce3bb12d9d9bd2907556f751c99c211e27b2
Jens Nazarenus <jens@0x6a.de>
2013-07-07 18:45:48 +0200

    added copyright infos

--- src/Osrm/OsrmClient.php
+++ src/Osrm/OsrmClient.php
@@ -1,4 +1,21 @@
 <?php
+/* 
+ * osrm-api-client, a PHP implementation of the OSRM server API 
+ * Copyright (C) 2013 Jens Nazarenus
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace Osrm;
 
@@ -32,7 +49,7 @@ class OsrmClient {
         ));
         
         $resp = curl_exec($curl);
-        echo($resp);
+        
         curl_close($curl);
         $json = json_decode($resp);
         
